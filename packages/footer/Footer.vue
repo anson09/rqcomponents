83c0bc23719be169616ac27afc4554d8afb8b5a8
singing <tang.xinying@ricequant.com>
2020-11-11 17:18:42 +0800

    feat: rq footer

--- packages/footer/Footer.vue
+++ packages/footer/Footer.vue
@@ -1,21 +1,19 @@
 <template>
-  <footer :class="['footer', { opacity }]">
-    <div class="footer__content">
-      <div class="footer__content-wrapper">
+  <footer :class="['rq-footer', { 'is-opacity': opacity }]">
+    <div class="rq-footer-menu-wrapper">
+      <div class="rq-footer-menu">
         <div
           v-for="(item, index) in links"
           :key="index"
-          class="footer__content-item"
+          class="rq-footer-menu__item"
         >
           <p
             v-for="(subitem, subIndex) in item"
             :key="subIndex"
             :class="[
-              subitem.title
-                ? 'footer__content-item__title'
-                : 'footer__content-item__label',
+              subitem.title ? 'rq-footer-menu__title' : 'rq-footer-menu__label',
 
-              { link: typeof subitem === 'object' && subitem.link },
+              { 'is-link': typeof subitem === 'object' && subitem.link },
             ]"
             @click="handleLink(subitem.link || '')"
           >
@@ -23,15 +21,15 @@
           </p>
         </div>
 
-        <div class="footer__content-contact">
+        <div class="rq-footer-contact">
           <span
             v-for="(item, index) in contact"
             :key="index"
-            class="footer__content-contact-item"
+            class="rq-footer-contact__item"
           >
             <img
               v-if="item.qrcode"
-              class="footer__content-contact-item__img"
+              class="rq-footer-contact__item-img"
               :src="qrcode"
             />
 
@@ -43,12 +41,12 @@
         </div>
       </div>
     </div>
-    <div class="footer__copy-right">
+    <div class="rq-footer-copy-right">
       <span
         v-for="(item, index) in copyRight"
         :key="index"
         :class="{
-          'footer__copy-right-link': typeof item === 'object' && item.link,
+          'rq-footer-copy-right__link': typeof item === 'object' && item.link,
         }"
         @click="handleLink(item.link)"
         >{{ item.label || item }}</span
@@ -79,117 +77,22 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-@import "../common/style/mixins";
+@import "./style/mixins.scss";
 
-@mixin hover {
-  cursor: pointer;
-  &:hover {
-    color: rqthemify(--light-primary-color);
-  }
-}
-.footer {
+@include reset;
+
+@include block(null) {
   width: 100%;
   color: rqthemify(--text-normal);
   font-size: 12px;
   line-height: 1.5;
-  &__content {
-    border-bottom: 1px solid rqthemify(--border-primary);
-    &-wrapper {
-      padding: 20px 0;
-      @include desktop {
-        max-width: 1170px;
-      }
-      @include laptop {
-        max-width: 900px;
-      }
-      @include f-center;
-      margin: 0 auto;
-      @include pad {
-        justify-content: space-evenly;
-      }
-    }
-    &-item {
-      @include desktop {
-        margin-right: 108px;
-      }
-      @include laptop {
-        margin-right: 50px;
-      }
-      @include pad {
-        margin-right: 0;
-      }
-      &__title,
-      &__label {
-        margin: 10px 0;
-        width: max-content;
-      }
-      &__title {
-        font-weight: bold;
-      }
-      &__label {
-        &.link {
-          cursor: pointer;
-          font-weight: normal;
-          &:hover {
-            color: rqthemify(--primary-color);
-          }
-        }
-      }
-    }
-    &-contact {
-      .icon-base {
-        cursor: pointer;
-        font-size: 20px;
-        &:hover {
-          color: rqthemify(--text-primary);
-        }
-        &:active {
-          color: rqthemify(--light-primary-color);
-        }
-      }
-      display: flex;
-      @include pc {
-        flex: auto;
-      }
-
-      justify-content: flex-end;
-      align-items: flex-end;
-      &-item {
-        position: relative;
-        margin-right: 22px;
-        &:last-child {
-          margin-right: 0;
-        }
-
-        &__img {
-          padding: 12px;
-          box-shadow: 0 2px 12px 0 rqthemify(--shadow-primary);
-          position: absolute;
-          display: none;
-          top: -10px;
-          @include t-center-horizontal(-100%);
-          border: 1px solid rqthemify(--border-primary);
-          border-radius: 4px;
-          background-color: rqthemify(--white);
-        }
-        &:hover &__img {
-          display: block;
-        }
-      }
-    }
-  }
-  &__copy-right {
-    padding: 16px 0;
-    text-align: center;
-    &-link {
-      cursor: pointer;
-    }
-  }
-  &.opacity {
+  &.is-opacity {
     color: rqthemify(--text-white-7);
-    .footer__content {
+    @include block(menu) {
       border-color: rqthemify(--text-white-2);
-      &-contact .icon-base {
+    }
+    @include block(contact) {
+      .icon-base {
         color: rqthemify(--text-white-5);
         &:hover {
           color: rqthemify(--text-white-8);
@@ -199,6 +102,97 @@ export default {
         }
       }
     }
+  }
+}
+
+@include block(menu) {
+  position: relative;
+  max-width: $max-vw;
+  width: 100%;
+  padding: 20px 16px;
+  margin: 0 auto;
+  @include f-center(space-between, flex-start);
+  &-wrapper {
+    border-bottom: 1px solid rqthemify(--border-primary);
+  }
+
+  &__item {
+    p {
+      margin: 10px 0;
+      width: max-content;
+    }
+  }
+
+  &__label {
+    &.is-link {
+      cursor: pointer;
+      font-weight: normal;
+      &:hover {
+        color: rqthemify(--primary-color);
+      }
+    }
+    p {
+      margin: 10px 0;
+      width: max-content;
+    }
+  }
+
+  &__title {
+    font-weight: bold;
+  }
+}
+
+@include block(contact) {
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  align-self: flex-end;
+  .icon-base {
+    cursor: pointer;
+    font-size: 20px;
+    &:hover {
+      color: rqthemify(--text-primary);
+    }
+    &:active {
+      color: rqthemify(--light-primary-color);
+    }
+  }
+  @include pad {
+    position: absolute;
+    right: 16px;
+    bottom: -20px;
+    transform: translateY(-100%);
+  }
+
+  &__item {
+    position: relative;
+    margin-right: 22px;
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &-img {
+      padding: 12px;
+      box-shadow: 0 2px 12px 0 rqthemify(--shadow-primary);
+      position: absolute;
+      display: none;
+      top: -10px;
+      @include t-center-horizontal(-100%);
+      border: 1px solid rqthemify(--border-primary);
+      border-radius: 4px;
+      background-color: rqthemify(--white);
+    }
+    &:hover &-img {
+      display: block;
+    }
+  }
+}
+
+@include block(copy-right) {
+  padding: 16px 0;
+  text-align: center;
+  &-link {
+    cursor: pointer;
   }
 }
 </style>
